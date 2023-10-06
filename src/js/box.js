e8f17478f8b1b8d9e4dec52dfa3599299ee8a0cb
MitchK03 <mitchellkastein@gmail.com>
2023-10-05 20:07:50 -0500

    new js, webpack

---
+++ src/js/box.js
@@ -0,0 +1,12 @@
+import { boxEl } from "./elements"
+
+export function changeBoxColor() {
+    const currBgColor = boxEl.style.backgroundColor
+     if(currBgColor === 'yellow'){
+        boxEl.style.backgroundColor = 'blue'
+    }
+    else {
+        boxEl.style.backgroundColor = 'yellow'
+    }
+}
+
---
+++ src/js/elements.js
@@ -0,0 +1,4 @@
+export const boxBtn = document.getElementById('boxBtn')
+export const headerBtn = docuemnt.getElementById('headerBtn')
+export const boxEl = document.getElementById('box')
+export const headerEl = document.getElementById('header')
